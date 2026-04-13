0345b860944980d63da2f63e892789db67a89679
Mac Book <robelg@Robels-MBP.grinnell.edu>
2026-04-12 21:58:00 -0500

    DOne

--- src/main/java/edu/ttap/intmap/IntegerMaps.java
+++ src/main/java/edu/ttap/intmap/IntegerMaps.java
@@ -3,6 +3,25 @@ package edu.ttap.intmap;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Books tested:
+ * 1. War and Peace by Leo Tolstoy - http://www.gutenberg.org/ebooks/2600
+ *    Observation: The letter frequencies are consistent with known English frequency patterns.
+ *    'e' is the most common letter, followed by 't' and 'a', which matches.
+ * Using reportcounts.
+ * 
+ * countChars tested on:
+ * 2. Plato's Republic: https://www.gutenberg.org/ebooks/39493 
+ *    Found 154 unique charcters 
+ *  The techinqure from part 1 (using c - 'a') as an index) would not work for this
+ *  book because it doesnt contain non-English characters with larger character val.
+ *  (greek letters up to value 974) with would cause array index out of bounds.  
+ * 
+ */
+
 
 public class IntegerMaps {
 
@@ -27,8 +46,46 @@ public class IntegerMaps {
             char letter = (char) ('a' + i);
             System.out.println(letter + ": " + counts[i]);
         }
+    
     }
+
+    /**
+     * Counts and reports the number of unique character in a text file,
+     * its case-sensitive and includes all character(spaces, punctiuation and etc..)
+     * @param path the file path to the text file
+     * @return the number of unque characters found 
+     */
+
+    public static int countChars(String path) throws IOException {
+        // use tree set instead, to store the unique characters 
+        Set<Character> uniqueChars = new TreeSet<>();
+
+        Scanner scanner = new Scanner(new File(path));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            for( char c: line.toCharArray()) {
+                uniqueChars.add(c); // so we already handeled dups
+            }
+            
+        }
+        scanner.close();
+
+        // print all on one line 
+        for(char c : uniqueChars) {
+            System.out.print(c + ": " + (int) c + " ");
+        }
+        System.out.println();
+
+        return uniqueChars.size();
+    }
+
+
+
+
+
     public static void main(String[] args) throws IOException{
         reportCounts(args[0]);
+
+        System.out.println("Unique chars: " + countChars(args[0]));
     }   
 }
