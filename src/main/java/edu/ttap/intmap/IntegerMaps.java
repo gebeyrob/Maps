bbc0d54d9c9f5c635799c23cdb808f55a5513d26
Mac Book <robelg@Robels-MBP.grinnell.edu>
2026-04-12 20:49:33 -0500

    Done with count

--- src/main/java/edu/ttap/intmap/IntegerMaps.java
+++ src/main/java/edu/ttap/intmap/IntegerMaps.java
@@ -1,5 +1,34 @@
 package edu.ttap.intmap;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+
 public class IntegerMaps {
-    
+
+    public static void reportCounts(String path) throws IOException{
+        int [] counts = new int[26];
+
+        Scanner scanner = new Scanner(new File(path));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            for (char c : line.toCharArray()) {
+                c = Character.toLowerCase(c);
+                if(c >= 'a' && c <='z'){
+                    counts[c - 'a']++;
+                }
+            }
+        }
+        scanner.close();
+
+        // print result 
+
+        for(int i = 0; i < 26; i++) {
+            char letter = (char) ('a' + i);
+            System.out.println(letter + ": " + counts[i]);
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        reportCounts(args[0]);
+    }   
 }
